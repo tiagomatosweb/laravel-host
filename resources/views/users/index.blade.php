a5b10eb6b5f88aa7dbffd979ae13d3ec2f5a6cf9
Tiago Matos <tiago@tiagomatos.com>
2024-10-10 09:44:27 +1100

    Listando usuários parte 1

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('page-title', 'Usuários')
+@section('content')
+    Lista de usuários
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function() {
     Route::get('/', function () {
         return view('home');
-    });
+    })->name('home');
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
 });
