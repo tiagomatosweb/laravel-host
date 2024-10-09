9b5b7e19d345c59947675c89890292e78b734b99
Tiago Matos <tiago@tiagomatos.com>
2024-10-09 11:34:05 +1100

    Cadastrando usuário parte 1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,28 @@
-Register
+@extends('layouts.auth')
+@section('body-class', 'register-page')
+@section('content')
+    <div class="register-box">
+        <div class="register-logo"> <a href="../index2.html"><b>Admin</b>LTE</a> </div>
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="register-box-msg">Register a new membership</p>
+                <form action="../index3.html" method="post">
+                    <div class="input-group mb-3"> <input type="text" class="form-control" placeholder="Full Name">
+                        <div class="input-group-text"> <span class="bi bi-person"></span> </div>
+                    </div>
+                    <div class="input-group mb-3"> <input type="email" class="form-control" placeholder="Email">
+                        <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
+                    </div>
+                    <div class="input-group mb-3"> <input type="password" class="form-control" placeholder="Password">
+                        <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                    </div>
+                    <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Sign In</button> </div>
+                </form>
+
+                <p class="mt-2 text-center"> <a href="login.html" class="text-center">
+                        I already have a membership
+                    </a> </p>
+            </div>
+        </div>
+    </div>
+@endsection
