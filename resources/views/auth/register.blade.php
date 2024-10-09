1682bb7fbd12df581219cca26a0d619a9256dfe5
Tiago Matos <tiago@tiagomatos.com>
2024-10-09 14:28:24 +1100

    Cadastrando usuário parte 3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,20 +9,35 @@
                 <form action="{{ route('register') }}" method="post">
                     @csrf
                     <div class="input-group mb-3">
-                        <input type="text" name="name" class="form-control" placeholder="Full Name">
                         <div class="input-group-text"> <span class="bi bi-person"></span> </div>
+                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Full Name" value="{{ old('name') }}">
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <input type="email" name="email" class="form-control" placeholder="Email">
                         <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}">
+                        @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="password" class="form-control" placeholder="Password">
                         <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
+                        @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div class="input-group mb-3">
+                        <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
                         <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation">
-                        <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
                     </div>
                     <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Register</button> </div>
                 </form>
