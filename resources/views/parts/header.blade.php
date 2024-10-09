36a89e50ff8db9a7b6e72bb3044f0aafaa27b24e
Tiago Matos <tiago@tiagomatos.com>
2024-10-10 09:01:49 +1100

    Fazendo logout

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -84,7 +84,13 @@
                             <div class="col-4 text-center"> <a href="#">Friends</a> </div>
                         </div>
                     </li>
-                    <li class="user-footer"> <a href="#" class="btn btn-default btn-flat">Profile</a> <a href="#" class="btn btn-default btn-flat float-end">Sign out</a> </li>
+                    <li class="user-footer">
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="btn btn-default btn-flat float-end">Logout</button>
+                        </form>
+                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                    </li>
                 </ul>
             </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function() {
+    Route::get('/', function () {
+        return view('home');
+    });
 });
