17d19ce5cf82f7ef565d7438986d5fa6096f3ef7
Tiago Matos <tiago@tiagomatos.com>
2024-10-09 14:50:42 +1100

    Fazendo login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,43 @@
-Login
+@extends('layouts.auth')
+@section('body-class',  'login-page ')
+@section('content')
+    <div class="login-box">
+        <div class="login-logo"> <a href="../index2.html"><b>Admin</b>LTE</a> </div> <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+                <form action="{{ route('login') }}" method="post">
+                    @csrf
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}">
+                        @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
+                        @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Login</button> </div>
+                </form>
+
+                <div class="mt-2 text-center">
+                    <p class="mb-1"> <a href="forgot-password.html">I forgot my password</a> </p>
+                    <p class="mb-0"> <a href="register.html" class="text-center">
+                            Register a new membership
+                        </a> </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
