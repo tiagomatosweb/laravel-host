8db2adc7cfa832437120ccea4ccd4ae0307d10ce
Tiago Matos <tiago@tiagomatos.com>
2024-10-09 14:16:31 +1100

    Cadastrando usuário parte 2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,17 +6,25 @@
         <div class="card">
             <div class="card-body register-card-body">
                 <p class="register-box-msg">Register a new membership</p>
-                <form action="../index3.html" method="post">
-                    <div class="input-group mb-3"> <input type="text" class="form-control" placeholder="Full Name">
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" name="name" class="form-control" placeholder="Full Name">
                         <div class="input-group-text"> <span class="bi bi-person"></span> </div>
                     </div>
-                    <div class="input-group mb-3"> <input type="email" class="form-control" placeholder="Email">
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control" placeholder="Email">
                         <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
                     </div>
-                    <div class="input-group mb-3"> <input type="password" class="form-control" placeholder="Password">
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control" placeholder="Password">
                         <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
                     </div>
-                    <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Sign In</button> </div>
+                    <div class="input-group mb-3">
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation">
+                        <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                    </div>
+                    <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Register</button> </div>
                 </form>
 
                 <p class="mt-2 text-center"> <a href="login.html" class="text-center">
