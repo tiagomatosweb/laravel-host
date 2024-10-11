94b2f9ecc4d5b47b98ad3fc21cb0ee9a550a9a94
Tiago Matos <tiago@tiagomatos.com>
2024-10-11 16:09:15 +1100

    Pegando dados do user logado

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -67,11 +67,11 @@
             <li class="nav-item"> <a class="nav-link" href="#" data-lte-toggle="fullscreen"> <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i> <i data-lte-icon="minimize" class="bi bi-fullscreen-exit" style="display: none;"></i> </a> </li>
             <li class="nav-item dropdown user-menu"> <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                     <img src="{{ Vite::asset('resources/images/user2-160x160.jpg') }}" class="user-image rounded-circle shadow" alt="User Image">
-                    <span class="d-none d-md-inline">Alexander Pierce</span> </a>
+                    <span class="d-none d-md-inline">{{ auth()->user()->name }}</span> </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                     <li class="user-header text-bg-primary"> <img src="{{ Vite::asset('resources/images/user2-160x160.jpg') }}" class="rounded-circle shadow" alt="User Image">
                         <p>
-                            Alexander Pierce - Web Developer
+                            {{ auth()->user()->name }}
                             <small>Member since Nov. 2023</small>
                         </p>
                     </li>
